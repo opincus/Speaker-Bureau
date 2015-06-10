bad8eb3802ced6da4038495072d58941df87f4a0
oliver@xlearnlab.net <oliver@xlearnlab.net>
2015-06-09 21:06:34 -0400

    Test

--- plugin.php
+++ plugin.php
@@ -795,14 +795,32 @@ function wds_handle_frontend_new_post_form_submission() {
 	// An array of IDs of categories we to add to this post.
 	// $cat_ids = array_map( 'intval', $cat_ids );
 	// $cat_ids = array_unique( $cat_ids );
+<<<<<<< HEAD
 		
+=======
+	
+>>>>>>> parent of a4a95c8... as
 	$cat_ids = array( 1 );
 	// Add these categories, note the last argument is true.
 	$term_taxonomy_ids = wp_set_object_terms( $new_submission_id, $cat_ids, 'category', true );
 	
+<<<<<<< HEAD
 	$cat_ids = array( 4,6 );
 	$term_taxonomy_ids = wp_set_object_terms( $new_submission_id, $cat_ids, 'topics', true );
 		
+=======
+    if ( $cmb->get_field( 'anl' ) == 'True' ) {
+		$cat_ids = array( 4 );
+		$term_taxonomy_ids = wp_set_object_terms( $new_submission_id, $cat_ids, 'topics', true );
+	}
+	
+	if ( $cmb->get_field( 'mae' ) == 'True' ) {
+		$cat_ids = array( 6 );
+		$term_taxonomy_ids = wp_set_object_terms( $new_submission_id, $cat_ids, 'topics', true );
+	}
+	
+			
+>>>>>>> parent of a4a95c8... as
 	// If we hit a snag, update the user
 	if ( is_wp_error( $new_submission_id ) ) {
 		return $cmb->prop( 'submission_error', $new_submission_id );
@@ -836,6 +854,7 @@ function wds_handle_frontend_new_post_form_submission() {
         wp_mail( $multiple_recipients, $subj, $body );
     
             
+<<<<<<< HEAD
     $multiple_recipients = array(
         'oliver@oliverpincus.com',
         'oliver@xlearnlab.net'
@@ -846,6 +865,13 @@ function wds_handle_frontend_new_post_form_submission() {
     
     
     
+=======
+		$multiple_recipients = array(
+        'oliver@oliverpincus.com' );
+        $subj = 'The email subject';
+        $body = 'This is the body of the email: ' . $cmb->get_field( 'anl' ) . $cmb->get_field( 'mae' );
+		wp_mail( $multiple_recipients, $subj, $body );
+>>>>>>> parent of a4a95c8... as
     
 	/*
 	 * Redirect back to the form page with a query variable with the new post ID.
